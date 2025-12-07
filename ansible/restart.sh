38a8b3f7b92578ca0d539ad3649e60fc2dbcd00e
Akfvh <raradong@naver.com>
2025-12-07 04:13:54 +0000

    update conf

--- ansible/restart.sh
+++ ansible/restart.sh
@@ -9,13 +9,13 @@ cd ~/Serverless/openwhisk/ansible
 #export ENVIRONMENT=local
 export ENVIRONMENT=distributed
 
-ansible-playbook -i environments/$ENVIRONMENT couchdb.yml; 
-ansible-playbook -i environments/$ENVIRONMENT initdb.yml; 
-ansible-playbook -i environments/$ENVIRONMENT wipe.yml; 
-ansible-playbook -i environments/$ENVIRONMENT openwhisk.yml; 
-ansible-playbook -i environments/$ENVIRONMENT postdeploy.yml; 
-ansible-playbook -i environments/$ENVIRONMENT apigateway.yml; 
-ansible-playbook -i environments/$ENVIRONMENT routemgmt.yml
+ansible-playbook -i environments/$ENVIRONMENT couchdb.yml -e  docker_image_tag=$OW_TAG_SODA
+ansible-playbook -i environments/$ENVIRONMENT initdb.yml -e  docker_image_tag=$OW_TAG_SODA
+ansible-playbook -i environments/$ENVIRONMENT wipe.yml -e  docker_image_tag=$OW_TAG_SODA
+ansible-playbook -i environments/$ENVIRONMENT openwhisk.yml -e  docker_image_tag=$OW_TAG_SODA
+ansible-playbook -i environments/$ENVIRONMENT postdeploy.yml -e  docker_image_tag=$OW_TAG_SODA
+ansible-playbook -i environments/$ENVIRONMENT apigateway.yml -e  docker_image_tag=$OW_TAG_SODA
+ansible-playbook -i environments/$ENVIRONMENT routemgmt.yml -e  docker_image_tag=$OW_TAG_SODA
 
 # deploy functions
 cd ~/benchmarks/serverless
